54e232b691ec1ae0be7f167126e33c68c85b5b31
Sean Stilwell <sstil051@uottawa.ca>
2021-06-01 20:18:53 -0400

    Adding lab 3

--- lab3/main.js
+++ lab3/main.js
@@ -88,21 +88,27 @@ function selectedItems(){
 	
 	var c = document.getElementById('displayCart');
 	c.innerHTML = "";
+	var checkout = document.getElementById('checkout');
 	
 	// build list of selected item
 	var para = document.createElement("P");
-	para.innerHTML = "You selected : ";
+	para.innerHTML = "You added the following items to your cart: ";
 	para.appendChild(document.createElement("br"));
 	for (i = 0; i < ele.length; i++) { 
 		if (ele[i].checked) {
 			para.appendChild(document.createTextNode(ele[i].value));
 			para.appendChild(document.createElement("br"));
 			chosenProducts.push(ele[i].value);
+			checkout.disabled = false;
 		}
 	}
 		
 	// add paragraph and total price
 	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(chosenProducts)));
+	c.appendChild(document.createTextNode("Your total price is $" + getTotalPrice(chosenProducts)));
 		
+}
+
+function rld(){
+	location.reload();
 }
